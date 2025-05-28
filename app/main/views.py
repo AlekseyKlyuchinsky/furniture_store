aaee3c0f5a12bb9ae854d56efb4528a561d3e2bb
Aleksey Klyuchinsky <klychinskyia@gmail.com>
2025-05-28 12:28:31 +0300

    Created 'about.html' and maked link for Home and About

--- app/main/views.py
+++ app/main/views.py
@@ -4,12 +4,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Main page of store'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context=context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'About - О нас',
+        'content': 'About us',
+        'text_on_page': 'sample'
+    }
+    return render(request, 'main/about.html', context=context)
