f716a207ad692b0778f365f32716ee353146b213
Aleksey Klyuchinsky <klychinskyia@gmail.com>
2025-07-23 13:09:32 +0300

    after adding from fixtures to goods

--- app/goods/models.py
+++ app/goods/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table: str = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='описание')
+    image = models.ImageField(upload_to='foods_images', blank=True, null=True, verbose_name='изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='цена')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='скидка в процентах')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='категория')
+
+    class Meta:
+        db_table: str = 'product'
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
