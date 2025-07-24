f7f4acf93ec3e229493c43caaf1918ad924e0eec
Aleksey Klyuchinsky <klychinskyia@gmail.com>
2025-07-24 11:54:32 +0300

    Registered simple tag for categories

--- app/goods/models.py
+++ app/goods/models.py
@@ -18,7 +18,7 @@ class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='описание')
-    image = models.ImageField(upload_to='foods_images', blank=True, null=True, verbose_name='изображение')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='цена')
     discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='скидка в процентах')
     quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
@@ -31,3 +31,12 @@ class Products(models.Model):
 
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
+
+    def display_id(self):
+        return f'{self.id:05}'
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount/100, 2)
+
+        return self.price
